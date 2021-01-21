b9f1b99c97123e19e1ffde551ad07be7bf04dbfe
Tristan Hall <tristanh@mm.services>
2021-01-21 13:09:37 -0500

    Rename package.
    Signed-off-by: Tristan Hall <tristanh@mm.services>

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -20,7 +20,7 @@ class ApiClient
 
     const USER_AGENT = 'mms-pdftk-php-client';
 
-    const VERSION = '0.1.0';
+    const VERSION = '0.1.1';
 
     /**
      * The API URL to use for HTTP requests.
