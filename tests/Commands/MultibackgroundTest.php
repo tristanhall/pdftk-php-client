8d698ad38c974cd571b40b96c7eadea55e2f1af8
Malcolm Hall <malcolmh@mm.services>
2021-02-16 11:10:56 -0500

    Add unit tests for Multibackground command class.
    Signed-off-by: Malcolm Hall <malcolmh@mm.services>

--- tests/Commands/MultibackgroundTest.php
+++ tests/Commands/MultibackgroundTest.php
@@ -2,9 +2,22 @@
 
 namespace Tests\Commands;
 
+use MinuteMan\PdftkClient\Commands\Multibackground;
 use PHPUnit\Framework\TestCase;
 
 class MultibackgroundTest extends TestCase
 {
+
+    /**
+     * Test for getCommandName()
+     */
+    public function testGetCommandName()
+    {
+        $cmd = new Multibackground();
+        $result = $cmd->getCommandName();
+
+        $this->assertIsString($result);
+        $this->assertEquals('multibackground', $result);
+    }
 
 }
