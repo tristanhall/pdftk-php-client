89bb344969e6f464facdedd68187c6718654f1ba
Malcolm Hall <malcolmh@mm.services>
2021-02-16 11:12:47 -0500

    Add unit tests for Multistamp command class.
    Signed-off-by: Malcolm Hall <malcolmh@mm.services>

--- tests/Commands/MultistampTest.php
+++ tests/Commands/MultistampTest.php
@@ -2,9 +2,27 @@
 
 namespace Tests\Commands;
 
+use MinuteMan\PdftkClient\Commands\Multistamp;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class MultistampTest
+ *
+ * @package Tests\Commands
+ */
 class MultistampTest extends TestCase
 {
+
+    /**
+     * Test for getCommandName()
+     */
+    public function testGetCommandName()
+    {
+        $cmd = new Multistamp();
+        $result = $cmd->getCommandName();
+
+        $this->assertIsString($result);
+        $this->assertEquals('multistamp', $result);
+    }
 
 }
