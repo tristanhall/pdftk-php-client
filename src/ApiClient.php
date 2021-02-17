f1da2b90f6f4837787d85aa6416783a54d169b7e
Tristan Hall <tristanh@mm.services>
2021-02-17 17:24:43 -0500

    Merge MMS-539 with unit tests for adding and removing flags and options.
    Signed-off-by: Tristan Hall <tristanh@mm.services>

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -20,7 +20,7 @@ class ApiClient
 
     const USER_AGENT = 'mms-pdftk-php-client';
 
-    const VERSION = '0.1.6';
+    const VERSION = '0.1.7';
 
     /**
      * The API URL to use for HTTP requests.
